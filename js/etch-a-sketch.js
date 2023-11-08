9daf900bb3f11aba6b8e667189ace007aed5f6ee
obeanddev <132011354+obeanddev@users.noreply.github.com>
2023-11-08 22:30:52 +0100

    Add a non complete hover effect

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -3,8 +3,9 @@ const COLORS=[
             "bisque","black","blue","brown",
             "cadetblue","cyan","darkseagreen",
             "gray","green","orange",
-            "red","salmon","turquoise","yellow","white"
+            "red","salmon","turquoise","yellow"
 ];
+const lengthStyleRegEx=/(\d*\.?\d*)([a-z]*)/i;
 function chooseColor()  {
     return COLORS[Math.floor(Math.random() *COLORS.length)];
 
@@ -14,10 +15,7 @@ function createSquare(sideLength) {
     squareEl.style.width=squareEl.style.height=sideLength;
     squareEl.classList.add("square");
     containerEl.appendChild(squareEl);
-    squareEl.addEventListener("click",(e)=> {
-       squareEl.style.backgroundColor=chooseColor();
-
-    });
+    
 
     
 }
@@ -31,11 +29,11 @@ function createGridOfSquares(nSquareBySide) {
      * followed by a possibly empty string of digits
      * followed by a unit of measure(px, pt, ...) 
     */
-    const heightMatch = height.match(/(\d*\.?\d*)([a-z]*)/i);
-    if (heightMatch.length ===3) {
+    const heightStrings = height.match(lengthStyleRegEx);
+    if (heightStrings.length ===3) {
         //item at index [0] is the whole string
         //item at index[1] is the number of units
-        squareSide=Math.floor(Number(heightMatch[1])/nSquareBySide) + heightMatch[2] ;
+        squareSide=Number(heightStrings[1])/nSquareBySide + heightStrings[2] ;
 
         //item at index[2] is the unit;
 
@@ -48,4 +46,42 @@ function createGridOfSquares(nSquareBySide) {
 
 
 }
-createGridOfSquares(10);
+function changeSquareColor(e) {
+    console.log("relatedTarget",e.relatedTarget);
+    if (    e.relatedTarget!==null &&
+            e.relatedTarget.tagName !=="BODY" &&
+            e.relatedTarget.tagName!=="HTML"              
+    ) {
+        e.relatedTarget.style.backgroundColor=chooseColor();
+    }
+
+
+}
+//Doesn't work when the mouse comes from body
+containerEl.addEventListener("mouseover",changeSquareColor);
+containerEl.addEventListener("mousemove",(e)=>{
+    /* we are going to work with the coordinates relative
+     * to the parent div containerEl 
+     * here there is no related target
+    * 
+    */ 
+   
+    const squareStyles=window.getComputedStyle(containerEl.firstChild);
+    const squareSide=squareStyles.getPropertyValue("height");
+    const squareStrings=squareSide.match( lengthStyleRegEx);
+    
+    if (squareStrings.length === 3) {
+        let squareSideUnitLess = squareStrings[1];
+        //get the size of the container
+        //divide the container height by square height
+        // then we have the number of square by side
+        // we compare this values using maybe the % operator    
+        // and with client x and y we calculate on which square we are,
+        // then we can change square colors
+
+    }
+
+
+
+});
+createGridOfSquares(20);
