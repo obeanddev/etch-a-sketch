3a1a57c3bd3deeb70961734b358bbf566a7a9609
obeanddev <132011354+obeanddev@users.noreply.github.com>
2023-11-10 09:10:02 +0100

    Add working mouse hover effect

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,87 +1,159 @@
-const containerEl=document.querySelector("#container");
-const COLORS=[
-            "bisque","black","blue","brown",
-            "cadetblue","cyan","darkseagreen",
-            "gray","green","orange",
-            "red","salmon","turquoise","yellow"
+
+const COLORS = [
+    "bisque", "black", "blue", "brown",
+    "cadetblue", "cyan", "darkseagreen",
+    "gray", "green", "orange",
+    "red", "salmon", "turquoise", "yellow"
 ];
-const lengthStyleRegEx=/(\d*\.?\d*)([a-z]*)/i;
-function chooseColor()  {
-    return COLORS[Math.floor(Math.random() *COLORS.length)];
+/** * 
+ * @param {Number} min lower bound, if it's not an integer, will be raised to the next integer
+ * @param {Number} max greater bound, if it's not an integer, will be lowered to the previous integer
+ * @returns {Number} 
+ * @description returns a integer value between min included 
+ * and max excluded
+ */
+function getRandomInt(min, max) {
+    /* see MDN page on Math.random */
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function chooseColor() {
+    /* return COLORS[Math.floor(Math.random() * COLORS.length)];*/
+    const red = getRandomInt(0, 256);
+    const green = getRandomInt(0, 256);
+    const blue = getRandomInt(0, 256);
+    return `rgb(${red},${green},${blue})`;
 
 }
-function createSquare(sideLength) {
-    const squareEl=document.createElement("div");
-    squareEl.style.width=squareEl.style.height=sideLength;
+function createSquare(container, sideLength) {
+    const squareEl = document.createElement("div");
+    squareEl.style.width = squareEl.style.height = sideLength;
     squareEl.classList.add("square");
-    containerEl.appendChild(squareEl);
-    
-
-    
+    container.appendChild(squareEl);
 }
-function createGridOfSquares(nSquareBySide) {
-    const containerStyles= window.getComputedStyle(containerEl);
-    const height = containerStyles.getPropertyValue("height");
-    let squareSide="100px";
-    /* height contains the unit of measure
+/**
+ * @param {*} element the element of which we want to obtain a property
+ * @param {*} propertyName the propertName of a length property
+ * @returns if it an array of two values then it is [unitLessLenght,lenghtUnit],
+ * else the first element is the whole styleLength string
+ */
+function splitStyleLength(element, propertyName) {
+    /* styleLength contains the unit of measure
      * we search for a pattern of the form: 
-     *  possibly empty string of digits followed by an eventual decimal point
+     * possibly empty string of digits followed
+     * by an eventual decimal point
      * followed by a possibly empty string of digits
      * followed by a unit of measure(px, pt, ...) 
     */
-    const heightStrings = height.match(lengthStyleRegEx);
-    if (heightStrings.length ===3) {
+    const styleLengthRegEx = /(\d*\.?\d*)([a-z]*)/i;
+
+    const elementStyles = window.getComputedStyle(element);
+    const propertyValue = elementStyles.getPropertyValue(propertyName);
+    const styleLengthArray = propertyValue.match(styleLengthRegEx);
+    if (styleLengthArray.length === 3) {
+        // styleLengthArray[0] is the whole string
+        // styleLengthArray[1] is the number of units
+        // styleLengthArray[2] is the unit;
+        return styleLengthArray.slice(1);
+
+    } else {
+        return styleLengthArray;
+    }
+}
+/**
+ * 
+ * @param {*} container 
+ 
+ * @returns an array of two values then it is [unitLessLenght,lenghtUnit],
+ *          when there is more elements in the array, the first element is the whole styleLength string
+ * 
+ */
+function getSquareSide(container, nSquareBySide) {
+    let squareSide = "100px";
+    const heightStrings = splitStyleLength(container, "height");
+    if (heightStrings.length === 2) {
         //item at index [0] is the whole string
         //item at index[1] is the number of units
-        squareSide=Number(heightStrings[1])/nSquareBySide + heightStrings[2] ;
-
-        //item at index[2] is the unit;
-
+        squareSide = Number(heightStrings[0]) / nSquareBySide + heightStrings[1];
     }
-    for(let i=1; i <= nSquareBySide ** 2;i++) {
-        createSquare(squareSide);
+    return squareSide;
+}
+
+function createGridOfSquares(container, nSquareBySide) {
+    const squareSide = getSquareSide(container, nSquareBySide);
+    for (let i = 1; i <= nSquareBySide ** 2; i++) {
+        createSquare(container, squareSide);
     }
-
-
-
 
 }
-function changeSquareColor(e) {
-    console.log("relatedTarget",e.relatedTarget);
-    if (    e.relatedTarget!==null &&
-            e.relatedTarget.tagName !=="BODY" &&
-            e.relatedTarget.tagName!=="HTML"              
+
+function changeSquareColor(squareEl) {
+
+    if (squareEl !== null &&
+        squareEl.tagName !== "BODY" &&
+        squareEl.tagName !== "HTML"
     ) {
-        e.relatedTarget.style.backgroundColor=chooseColor();
+        squareEl.style.backgroundColor = chooseColor();
     }
 
 
 }
-//Doesn't work when the mouse comes from body
-containerEl.addEventListener("mouseover",changeSquareColor);
-containerEl.addEventListener("mousemove",(e)=>{
+const containerEl = document.querySelector("#container");
+let previousVisitedSquareEl = null;
+
+containerEl.addEventListener("mouseleave", (e) => {
+    previousVisitedSquareEl == null;
+
+
+});
+containerEl.addEventListener("mousemove", (e) => {
     /* we are going to work with the coordinates relative
      * to the parent div containerEl 
      * here there is no related target
     * 
-    */ 
-   
-    const squareStyles=window.getComputedStyle(containerEl.firstChild);
-    const squareSide=squareStyles.getPropertyValue("height");
-    const squareStrings=squareSide.match( lengthStyleRegEx);
-    
-    if (squareStrings.length === 3) {
-        let squareSideUnitLess = squareStrings[1];
-        //get the size of the container
-        //divide the container height by square height
-        // then we have the number of square by side
-        // we compare this values using maybe the % operator    
-        // and with client x and y we calculate on which square we are,
-        // then we can change square colors
-
+    */
+    const squareSideArray = splitStyleLength(containerEl.firstChild, "height");
+    const containerLengthArray = splitStyleLength(containerEl, "height");
+    if (squareSideArray.length === 2 && containerLengthArray.length === 2) {
+        let squareSideUnitLess = squareSideArray[0];
+        let squareSideUnits = squareSideArray[1];
+        let containerLengthUnitLess = containerLengthArray[0];
+        let containerLengthUnits = containerLengthArray[1];
+        if (squareSideUnits === containerLengthUnits) {
+            let nSquareBySide = containerLengthUnitLess / squareSideUnitLess;
+            /* offsetX, offsetY are relative to the target element 
+             * that is a square div here
+             * and that 's not what we want
+             * we want values relative to the container 
+             */
+            let row = Math.floor((e.clientY) / squareSideUnitLess);
+            let col = Math.floor((e.clientX) / squareSideUnitLess);
+            /* would e.pageX, e.pageY be the right properties to compute with ??*/
+            let visitedSquareEl;
+            //we calculate on which square we are on
+            const squareNumber = row * nSquareBySide + (col + 1);
+            visitedSquareEl = containerEl.querySelector(
+                `div:nth-of-type(${squareNumber}`);
+            /* console.log("row", row, "col", col);
+             console.log("target", e.target);
+             console.log("currentTarget", e.currentTarget);
+            */
+            if (visitedSquareEl.classList.contains("square")) {
+                if (previousVisitedSquareEl === null) {
+                    previousVisitedSquareEl = visitedSquareEl;
+                    changeSquareColor(visitedSquareEl);
+                } else {
+                    let sameNode = previousVisitedSquareEl.isSameNode(visitedSquareEl)
+                    if (!sameNode) {
+                        changeSquareColor(visitedSquareEl);
+                        previousVisitedSquareEl = visitedSquareEl;
+                        //visitedSquareEl.innerText=row* nSquareBySide +(col+1);
+                    }                    
+                }
+            }
+        }
     }
-
-
-
 });
-createGridOfSquares(20);
+
+createGridOfSquares(containerEl, 20);
