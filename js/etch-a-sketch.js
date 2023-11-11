9dfbb55a9611503226e95d78c1079237b344278a
obeanddev <132011354+obeanddev@users.noreply.github.com>
2023-11-11 19:47:39 +0100

    Clean the code of obsolete comments

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -53,7 +53,6 @@ function splitStyleLength(element, propertyName) {
         // styleLengthArray[1] is the number of units
         // styleLengthArray[2] is the unit;
         return styleLengthArray.slice(1);
-
     } else {
         return styleLengthArray;
     }
@@ -61,10 +60,9 @@ function splitStyleLength(element, propertyName) {
 /**
  * 
  * @param {*} container the element in which we want to create a square grid
- 
+ * @param {*} nSquareBySide the number of square by side, must be an integer
  * @returns an array of two values then it is [unitLessLenght,lenghtUnit],
  *          when there is more elements in the array, the first element is the whole styleLength string
- * 
  */
 function getSquareSide(container, nSquareBySide) {
     let squareSide = "100px";
@@ -84,18 +82,15 @@ function getOffsetLeftTop(fromEl, toEl) {
         el.offsetParent !== undefined &&
         el.offsetParent !== null
     ) {
-
         el = el.offsetParent;
         if (el) {
             offsets.offsetLeft += el.offsetLeft;
             offsets.offsetTop += el.offsetTop;
         }
-
     }
-
     return offsets;
-
 }
+
 function createGridOfSquares(container, nSquareBySide) {
     const squareSide = getSquareSide(container, nSquareBySide);
     let squareEl;
@@ -107,7 +102,6 @@ function createGridOfSquares(container, nSquareBySide) {
         col = (i - row * nSquareBySide - 1) % nSquareBySide;
         squareEl.setAttribute("data-row", row);
         squareEl.setAttribute("data-col", col);
-
     }
 }
 
@@ -124,12 +118,10 @@ let previousVisitedSquareEl = null;
 
 containerEl.addEventListener("mouseleave", (e) => {
     previousVisitedSquareEl = null;
-
-
 });
-containerEl.addEventListener("click", (e) => {
+/* containerEl.addEventListener("click", (e) => {
     console.log(e);
-})
+}) */
 containerEl.addEventListener("mousemove", (e) => {
     /* we are going to work with the coordinates relative
      * to the parent div containerEl 
@@ -143,83 +135,25 @@ containerEl.addEventListener("mousemove", (e) => {
         let containerLengthUnitLess = containerLengthArray[0];
         let containerLengthUnits = containerLengthArray[1];
         if (squareSideUnits === containerLengthUnits) {
-            let nSquareBySide = containerLengthUnitLess / squareSideUnitLess;
+            let nSquareBySide = Math.floor(containerLengthUnitLess / squareSideUnitLess);
             /* offsetX, offsetY are relative to the target element 
              * that is a square div here
              * and that 's not what we want
              * we want values relative to the container 
              */
-            let rectangles = containerEl.getClientRects();
-            let boundingRectangle = containerEl.getBoundingClientRect();
-            if (rectangles === undefined || boundingRectangle === undefined) {
-                return;
-            }
-            // I have to substact some thing on these two lines but I don't knwo what
             const offsets = getOffsetLeftTop(document.querySelector("body"), containerEl);
-
-            //console.log(offsets);
-            let zoom = window.devicePixelRatio;
-            // console.log("zoom: ", zoom);
-            let delta;
-            let magicVal
-            //  0.93275625 0.932953125  0.9328549375     0.9329040313 0.9328794844
-            //let row = Math.floor((e.pageY + Math.floor(squareSideUnitLess/(zoom*zoom)) - (rectangles[0].top + window.scrollY +1/(zoom*zoom) + delta)) / squareSideUnitLess);
-            //let col = Math.floor((e.pageX + Math.floor(squareSideUnitLess/(zoom*zoom)) - (rectangles[0].left + window.scrollX+1/(zoom*zoom) + delta)) / squareSideUnitLess );
-            let row;
-            let col;
-            delta = 0.960000000000015;
-            magicVal = 8.90625;
-            if (zoom > 1) {
-                delta = 3.78125;
-                let deltaProduct = squareSideUnitLess * delta;
-                ;
-                console.log("deltaproduct", deltaProduct);
-                // row= Math.floor((e.pageY + window.screenY -(rectangles[0].top + window.scrollY))/(squareSideUnitLess* zoom));
-                row = Math.floor((e.pageY - (offsets.offsetTop)) / squareSideUnitLess);
-                console.log((e.pageY - (offsets.offsetTop)) / squareSideUnitLess);
-                console.log("row", row);
-                console.log(e);
-                console.log("***********************");
-                //col= Math.floor((e.pageX + window.screenX-(rectangles[0].top + window.scrollX))/squareSideUnitLess * zoom);
-                col = Math.floor((e.pageX - (offsets.offsetLeft)) / squareSideUnitLess);
-                console.log((e.pageX - (offsets.offsetLeft)) / squareSideUnitLess);
-                console.log("col", col);
-                console.log("########################################");
-            } else if (zoom < 1) {
-                // row = Math.floor(((e.pageY + (squareSideUnitLess - magicVal * (1-zoom))) - (rectangles[0].top + window.scrollY  + delta)) / squareSideUnitLess);
-                // col = Math.floor(((e.pageX + (squareSideUnitLess- magicVal * (1-zoom))) - (rectangles[0].left + window.scrollX + delta)) / squareSideUnitLess );
-                //   row= Math.floor((e.pageY  -(rectangles[0].top ))/squareSideUnitLess);
-                // col= Math.floor((e.pageX  -(rectangles[0].left ))/squareSideUnitLess);
-                row = Math.floor((e.pageY - (offsets.offsetTop)) / squareSideUnitLess);
-                col = Math.floor((e.pageX - (offsets.offsetLeft)) / squareSideUnitLess);
-            } else {
-                /* if zoom===1 then
-                     e.pageX=e.clientX && e.pageY = e.clientY   
-
-
-                */
-                row = Math.floor((e.pageY - (offsets.offsetTop)) / squareSideUnitLess);
-                col = Math.floor((e.pageX - (offsets.offsetLeft)) / squareSideUnitLess);
-                /*magicVal+=0.625*squareSideUnitLess;
-                row = Math.floor(((e.pageY + (squareSideUnitLess - magicVal )) - (rectangles[0].top + window.scrollY  + delta)) / squareSideUnitLess );
-                col = Math.floor(((e.pageX + (squareSideUnitLess- magicVal )) - (rectangles[0].left + window.scrollX + delta)) / squareSideUnitLess  );
-                */
-            }
-            row = (row < 0) ? 0 : ((row < nSquareBySide) ? row : nSquareBySide - 1);
-            col = (col < 0) ? 0 : ((col < nSquareBySide) ? col : nSquareBySide - 1);
-            console.log("------------------------------------------------");
-            console.log({ row: row, col: col });
-            /* would e.pageX, e.pageY be the right properties to compute with ??*/
+            let row = Math.floor((e.pageY - (offsets.offsetTop)) / squareSideUnitLess);
+            let col = Math.floor((e.pageX - (offsets.offsetLeft)) / squareSideUnitLess);
             let visitedSquareElOld;
             let visitedSquareEl;
-            let elements = document.elementsFromPoint(e.x, e.y);
-            for (const el of elements) {
-                if (el.classList.contains("square")) {
-                    visitedSquareEl = el;
-                }
+            let element = document.elementFromPoint(e.x, e.y);
+
+            if (element !== null && element!== undefined && element.classList.contains("square")) {
+                visitedSquareEl = element;
             }
+
             if (visitedSquareEl === null || visitedSquareEl === undefined) {
-                console.log("still null");
+             //   console.log("still null");
                 return;
             }
             //we calculate on which square we are on
@@ -236,7 +170,9 @@ containerEl.addEventListener("mousemove", (e) => {
                     if (!sameNode) {
                         changeSquareColor(visitedSquareEl);
                         previousVisitedSquareEl = visitedSquareEl;
-                        //visitedSquareEl.innerText=row* nSquareBySide +(col+1);
+                        if (visitedSquareEl === visitedSquareElOld) {
+                            visitedSquareEl.innerText = squareNumber;
+                        }
                     }
                 }
             } //end if it's a square
